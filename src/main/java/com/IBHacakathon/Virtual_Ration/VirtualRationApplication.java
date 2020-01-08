8e617af7fc483f48782991bfae5367195d9c3193
Sudhir Kumar <sudhir2677cool@gmail.com>
2020-01-08 23:20:24 +0530

    created repository for hackathon

---
+++ src/main/java/com/IBHacakathon/Virtual_Ration/VirtualRationApplication.java
@@ -0,0 +1,13 @@
+package com.IBHacakathon.Virtual_Ration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualRationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualRationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/IBHacakathon/Virtual_Ration/VirtualRationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IBHacakathon.Virtual_Ration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualRationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
